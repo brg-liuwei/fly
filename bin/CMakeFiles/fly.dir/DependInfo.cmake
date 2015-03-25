abf411d70aff369cad472a099b6717b28548ba41
root <root@adindex1.(none)>
2015-03-25 15:24:57 +0800

    modify nginx.ad.conf

---
+++ bin/CMakeFiles/fly.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/opt/fly/src/fy_alloc.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_alloc.c.o"
+  "/opt/fly/src/fy_conf.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_conf.c.o"
+  "/opt/fly/src/fy_connection.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_connection.c.o"
+  "/opt/fly/src/fy_event.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_event.c.o"
+  "/opt/fly/src/fy_framework.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_framework.c.o"
+  "/opt/fly/src/fy_info.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_info.c.o"
+  "/opt/fly/src/fy_logger.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_logger.c.o"
+  "/opt/fly/src/fy_md5.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_md5.c.o"
+  "/opt/fly/src/fy_modules.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_modules.c.o"
+  "/opt/fly/src/fy_time.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_time.c.o"
+  "/opt/fly/src/fy_util.c" "/opt/fly/bin/CMakeFiles/fly.dir/fy_util.c.o"
+  "/opt/fly/src/json4c/jc_alloc.c" "/opt/fly/bin/CMakeFiles/fly.dir/json4c/jc_alloc.c.o"
+  "/opt/fly/src/json4c/jc_type.c" "/opt/fly/bin/CMakeFiles/fly.dir/json4c/jc_type.c.o"
+  "/opt/fly/src/json4c/jc_wchar.c" "/opt/fly/bin/CMakeFiles/fly.dir/json4c/jc_wchar.c.o"
+  "/opt/fly/src/modules/fy_api_adfilter_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_api_adfilter_module.c.o"
+  "/opt/fly/src/modules/fy_api_adidx_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_api_adidx_module.c.o"
+  "/opt/fly/src/modules/fy_api_adlog_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_api_adlog_module.c.o"
+  "/opt/fly/src/modules/fy_api_adpos_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_api_adpos_module.c.o"
+  "/opt/fly/src/modules/fy_c_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_c_module.c.o"
+  "/opt/fly/src/modules/fy_fcgi_accept_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_fcgi_accept_module.c.o"
+  "/opt/fly/src/modules/fy_fcgi_finish_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_fcgi_finish_module.c.o"
+  "/opt/fly/src/modules/fy_fcgi_layout_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_fcgi_layout_module.c.o"
+  "/opt/fly/src/modules/fy_graph_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_graph_module.c.o"
+  "/opt/fly/src/modules/fy_http_count_module.c" "/opt/fly/bin/CMakeFiles/fly.dir/modules/fy_http_count_module.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_GNU_SOURCE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/fcgi/include"
+  "/usr/local/mxml/include"
+  "src/."
+  "src/./json4c"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
